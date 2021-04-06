c65549c1e1d094e411ffd1ee648373636a22e2e6
Giovani Lamastra <58533135+gilamastra@users.noreply.github.com>
2021-04-06 14:55:59 -0300

    Add files via upload

---
+++ exercicio18.js
@@ -0,0 +1,51 @@
+function numeroExt(num){
+    switch (num) {
+        case 0:
+            return "Zero"
+            break;
+        case 1:
+            return "Um"
+            break;
+        case 2:
+            return  "Dois"
+            break;
+        case 3:
+            return "Três"
+            break;
+        case 4:
+            return "Quatro"
+            break;
+        case 5:
+            return "Cinco"
+            break;
+        case 6:
+            return "Seis"
+            break;
+        case 7:
+            return "Sete"
+            break;
+        case 8:
+            return "Oito"
+            break;
+        case 9:
+            return "Nove"
+            break;
+        case 10:
+            return "Dez"
+            break
+        default:
+            return "Numero invalido"
+    }
+}
+
+console.log(numeroExt(1));
+console.log(numeroExt(2));
+console.log(numeroExt(3));
+console.log(numeroExt(4));
+console.log(numeroExt(5));
+console.log(numeroExt(6));
+console.log(numeroExt(7));
+console.log(numeroExt(8));
+console.log(numeroExt(9));
+console.log(numeroExt(10));
+console.log(numeroExt(11));
